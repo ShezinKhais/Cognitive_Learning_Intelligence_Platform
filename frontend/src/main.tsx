c2c3fc4f8c8278258f8675d8e5a39506720381f6
Hunain <152446305+hunainraza1@users.noreply.github.com>
2026-08-13 13:24:33 +0400

    Remove AdminConsole route from main.tsx
    Removed the AdminConsole route from the main application.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
 import App from './App.tsx'
-import AdminConsole from './pages/AdminConsole.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminConsole />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
